ee2a6732f8f5d8cb48b21458b97dc737c7e9b7d4
Connor Frendt <cjacksonfx@yahoo.com>
2019-01-14 11:46:30 -0800

    setting up for loop and if statement to loop through array

---
+++ filter.js
@@ -0,0 +1,10 @@
+const filteredArray = function(array) {
+  array.filter(word => {
+    for(let i = 0; i < array.length; i++) {
+      if(word.inclues('y' || 'Y') {
+  
+      });
+    }
+  });
+  
+};

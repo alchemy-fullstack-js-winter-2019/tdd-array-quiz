65b5f224d7dc0b91ccfb170539dfa9bb25f377ec
Lance Owen Merrill <Lanceom@hawaii.edu>
2019-01-14 12:11:32 -0800

    test past

--- test/index.test.js
+++ test/index.test.js
@@ -4,9 +4,8 @@ describe('test a function to filter and return a new sentence ', () => {
   it('removes any words with "y" in them and restructures the array to sentence', done => {
 
     const values = ['sky', 'hot', 'Yeti', 'green'];
-    newSent(values, () => {
-    //   expect(err).toBeFalsy();
-      expect(newSent(values)).toEqual(['Is it hot?', 'Is it green?']);
+    newSent(values, returnedValues => {
+      expect(returnedValues).toEqual(['Is it hot?', 'Is it green?']);
       done();
     });
   });

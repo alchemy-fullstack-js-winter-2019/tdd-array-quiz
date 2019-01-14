dace8a468229e1046987a0edc4ca21a944a4cc5d
Paige Gorry <paigeegorry@gmail.com>
2019-01-14 11:51:21 -0800

    fixed test and method to filter out words with letter y

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,13 @@
 const filter = require('../lib/index');
 
 describe('array methods', () => {
-  it('filters out every odd word from an array', () => {
+  it('filters out every word with a y from an array', () => {
     let arr = ['sky', 'hot', 'Yeti', 'green'];
     let filtered = filter(arr);
     expect(filtered).toEqual(['hot', 'green']);
   });
+  it('adds those words back into the array in the form of a question', () => {
+    
+  });
 });
+

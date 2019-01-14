0db0009f10d1f1510fe4cfe1cf0200755077eef6
Ivan <i.poblete915@gmail.com>
2019-01-14 12:09:00 -0800

    was not able to complete quiz

---
+++ test/index.test.js
@@ -0,0 +1,10 @@
+
+describe('removeWords', () => {
+  it('will remove any word with the letter Y or y', () => {
+    const words = ['sky', 'hot', 'Yeti', 'green'].includes(ele => {
+      return ele === [1, 3];
+    });
+
+    expect(words).toEqual([1, 3]);
+  });
+});

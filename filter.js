3ee2f19eec3d67f61a38f9edb7ec54ace503cae0
Connor Frendt <cjacksonfx@yahoo.com>
2019-01-14 11:59:21 -0800

    trying to add "Is it <word>?" to array

--- filter.js
+++ filter.js
@@ -2,8 +2,13 @@ const filteredArray = function(array) {
   array.filter(word => {
     for(let i = 0; i < array.length; i++) {
       if(word.includes('y' || 'Y')) {
-        array[i].splice();
+        array.splice(i, 1);
       }
+    }
+    for(let i = 0; i < array.length; i++) {
+      array.map(word => {
+        'Is that ' + word + '?';
+      });
     }
     return array;
   });

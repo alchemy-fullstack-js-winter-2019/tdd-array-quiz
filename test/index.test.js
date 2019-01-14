7ce67fa9a53e448be335cc8dd356e6027862ad6a
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-14 11:41:17 -0800

    finishing test methods

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,15 @@
 const { array } = require('../lib/index');
 
+var letter1 = 'Y';
+var letter2 = 'y';
+
+var word = ['sky', 'hot', 'Yeti', 'green'];
+
 describe('functional array methods', () => {
   it('takes an array of words and returns a new array that has removed any words that contain the letter Y or y. Has modified the original word to be a phrase of the form Is it <word>?', () => {
-    const newArray = 
+    const newArray = console.log(`Is it "${word.includes(!letter1 && !letter2)}`);
+    return newArray;
   });
+
+  expect(newArray).toEqual([['Is it hot?', 'Is it green?']);
 });

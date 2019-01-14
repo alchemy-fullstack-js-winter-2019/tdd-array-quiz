0eb7fcf2c194e822e4d9456f36b888e9e57688ea
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-14 11:52:07 -0800

    one of two tests is passing

--- test/index.test.js
+++ test/index.test.js
@@ -1,15 +1,18 @@
-const { array } = require('../lib/index');
+// eslint-disable-next-line no-unused-vars
+const { method } = require('../lib/index');
 
 var letter1 = 'Y';
 var letter2 = 'y';
 
 var word = ['sky', 'hot', 'Yeti', 'green'];
 
-describe('functional array methods', () => {
+describe('method', () => {
   it('takes an array of words and returns a new array that has removed any words that contain the letter Y or y. Has modified the original word to be a phrase of the form Is it <word>?', () => {
+    // eslint-disable-next-line no-console
     const newArray = console.log(`Is it "${word.includes(!letter1 && !letter2)}`);
     return newArray;
   });
 
-  expect(newArray).toEqual([['Is it hot?', 'Is it green?']);
+  // eslint-disable-next-line no-undef
+  expect(newArray).toEqual(['Is it hot?', 'Is it green?']);
 });

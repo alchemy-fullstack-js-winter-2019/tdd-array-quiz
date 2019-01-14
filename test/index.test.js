eb6fe965d3d0b936c836ba0703e2d133562eb4af
Tyler Corbett <thetylercorbett@gmail.com>
2019-01-14 11:55:45 -0800

    failing test written. beginning second method

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,19 @@
 const { removeY } = require('../lib/index');
+const { makePhrase } = require('../lib/index');
+
+const arr = ['sky', 'hot', 'Yeti', 'green'];
+
 
 describe('array method tests', () => {
     it('removes any words that contain Y or y', () => {
-        const arr = ['sky', 'hot', 'Yeti', 'green'];
         const results = removeY(arr);
 
         expect(results).toEqual([ 'hot', 'green' ]);
+    });
+    it('modifies original word to be a phrase "Is it <word>"', () => {
+        const results = makePhrase(arr);
+
+        expect(results).toEqual(['Is it sky?', 'Is it hot?', 'Is it Yet?', 'Is it green?']);
     });
 })
 

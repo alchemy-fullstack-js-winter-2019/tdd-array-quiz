ae1cb8ce57a627442f1ed6cb7ebe9e96e37c8d65
Kristin Hortsch <kristinhortsch@gmail.com>
2019-01-14 11:50:45 -0800

    cleaned up

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,7 @@
 const quizFilter = require('../lib/index.js');
 
 describe('quizFilter', () => {
-  it('can filter out words with both lower case and capital Y\'s and return the filtered array in the question format', () => {
+  it('can filter out words with both upper and lower case Y\'s and return the filtered array in the "Is it...?" question format', () => {
     const array = ['whY', 'yo', 'sky', 'red', 'winter'];
     const expectedArray = ['Is it red?', 'Is it winter?'];
     expect(quizFilter(array)).toEqual(expectedArray);

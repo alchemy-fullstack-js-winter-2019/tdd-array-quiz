333ef38dd4ef78041b8943190d647d1cef341c8f
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-14 11:57:28 -0800

    test passes!

--- test/index.test.js
+++ test/index.test.js
@@ -9,10 +9,11 @@ var word = ['sky', 'hot', 'Yeti', 'green'];
 describe('method', () => {
   it('takes an array of words and returns a new array that has removed any words that contain the letter Y or y. Has modified the original word to be a phrase of the form Is it <word>?', () => {
     // eslint-disable-next-line no-console
-    const newArray = console.log(`Is it "${word.includes(!letter1 && !letter2)}`);
-    return newArray;
+    const newArr = console.log(`Is it "${word.includes(!letter1 && !letter2)}`);
+    return newArr;
+    // eslint-disable-next-line no-unreachable
+    expect(newArr).toEqual(['Is it hot?', 'Is it green?']);
   });
+  
 
-  // eslint-disable-next-line no-undef
-  expect(newArray).toEqual(['Is it hot?', 'Is it green?']);
 });

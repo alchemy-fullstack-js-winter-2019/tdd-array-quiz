24db0f3fe31d7eeeeec0e66ba3a9bb8118e5f064
Kristin Hortsch <kristinhortsch@gmail.com>
2019-01-14 11:55:04 -0800

    added one more test word in the array

--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,7 @@ const quizFilter = require('../lib/index.js');
 
 describe('quizFilter', () => {
   it('can filter out words with both upper and lower case Y\'s and return the filtered array in the "Is it...?" question format', () => {
-    const array = ['whY', 'yo', 'sky', 'red', 'winter'];
+    const array = ['whY', 'yo', 'sky', 'red', 'winter', 'okay'];
     const expectedArray = ['Is it red?', 'Is it winter?'];
     expect(quizFilter(array)).toEqual(expectedArray);
   });

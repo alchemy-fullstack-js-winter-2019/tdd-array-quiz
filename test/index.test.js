f94295bfac56bdee5b10aa1d13894a4bf12a4f63
Tyler Corbett <thetylercorbett@gmail.com>
2019-01-14 11:33:50 -0800

    changed test and began first function

--- test/index.test.js
+++ test/index.test.js
@@ -3,7 +3,7 @@ const { removeY } = require('../lib/index');
 describe('array method tests', () => {
     it('removes any words that contain Y or y', () => {
         const arr = ['sky', 'hot', 'Yeti', 'green'];
-        const results = arr.removeY();
+        const results = removeY(arr);
 
         expect(results).toEqual(['hot', 'green']);
     });

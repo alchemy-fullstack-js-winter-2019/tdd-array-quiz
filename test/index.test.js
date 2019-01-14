23667e188b63c105883d34d39f3b2a0ddbae5231
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-14 11:33:01 -0800

    setting up tests

---
+++ test/index.test.js
@@ -0,0 +1,7 @@
+const { array } = require('../lib/index');
+
+describe('functional array methods', () => {
+  it('takes an array of words and returns a new array that has removed any words that contain the letter Y or y. Has modified the original word to be a phrase of the form Is it <word>?', () => {
+    const newArray = 
+  });
+});

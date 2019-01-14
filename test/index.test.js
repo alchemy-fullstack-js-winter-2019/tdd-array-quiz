ef81dc2f6d3418926423540443b3c163f4f2d8a4
Tyler Corbett <thetylercorbett@gmail.com>
2019-01-14 12:16:13 -0800

    fixed linting errors

--- test/index.test.js
+++ test/index.test.js
@@ -6,20 +6,20 @@ const arr = ['sky', 'hot', 'Yeti', 'green'];
 
 
 describe('array method tests', () => {
-    it('removes any words that contain Y or y', () => {
-        const results = removeY(arr);
+  it('removes any words that contain Y or y', () => {
+    const results = removeY(arr);
 
-        expect(results).toEqual(['hot', 'green']);
-    });
-    it('modifies original word to be a phrase "Is it <word>"', () => {
-        const results = makePhrase(arr);
+    expect(results).toEqual(['hot', 'green']);
+  });
+  it('modifies original word to be a phrase "Is it <word>"', () => {
+    const results = makePhrase(arr);
 
-        expect(results).toEqual(['Is it sky?', 'Is it hot?', 'Is it Yeti?', 'Is it green?']);
-    });
-    it('removes words that contain Y and adds it into the phrase', () => {
-        const results = makeFinal(arr);
+    expect(results).toEqual(['Is it sky?', 'Is it hot?', 'Is it Yeti?', 'Is it green?']);
+  });
+  it('removes words that contain Y and adds it into the phrase', () => {
+    const results = makeFinal(arr);
 
-        expect(results).toEqual(['Is it hot?', 'Is it green?'])
-    })
-})
+    expect(results).toEqual(['Is it hot?', 'Is it green?']);
+  });
+});
 

fb9099e1679ed0e4ada76f098da7a3e0523c0fe2
Connor Frendt <cjacksonfx@yahoo.com>
2019-01-14 11:49:38 -0800

    attempted to implement a filteredArray method

--- filter.js
+++ filter.js
@@ -1,10 +1,9 @@
 const filteredArray = function(array) {
   array.filter(word => {
     for(let i = 0; i < array.length; i++) {
-      if(word.inclues('y' || 'Y') {
-  
-      });
+      if(word.includes('y' || 'Y')) {
+        array[i].splice(i, 1);
+      }
     }
   });
-  
 };

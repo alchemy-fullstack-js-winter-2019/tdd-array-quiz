6fe83f1744fe2c5bf1ccaf99de093d20d63b0e5f
Connor Frendt <cjacksonfx@yahoo.com>
2019-01-14 11:36:58 -0800

    set up more initial files and set up skeleton for test

---
+++ filter.test.js
@@ -0,0 +1,7 @@
+describe('filters and "replaces" words', () => {
+  it('removes all words that contain y', () => {
+    const array = ['sky', 'hot', 'Yeti', 'green'];
+    const final = ['Is it hot?', 'Is it green?'];
+    expect().toEqual();
+  });
+});

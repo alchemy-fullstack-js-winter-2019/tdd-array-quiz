a72f2c22cbf49399ca7c0ab218a54c99390c17c7
Kate Dameron <katerj33@gmail.com>
2019-01-14 12:26:57 -0800

    test still returning undefined

--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,7 @@ const filterArr = require('../lib/index');
 
 describe('filterArr', () => {
   it('will filter the words "hot" and "green" from an array and return the phrases "is it hot?" and "is it green?"', () => {
-    const results = filterArr(['sky', 'hot', 'Yeti', 'green'], 'hot', 'green');
+    const results = filterArr(['sky', 'hot', 'Yeti', 'green'], 'y');
     expect(results).toEqual(['Is it hot?', 'Is it green?']);
   });
-});
+})

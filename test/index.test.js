4baedd014387a00940736907a9b4f6b0bd3fa401
Paige Gorry <paigeegorry@gmail.com>
2019-01-14 11:42:55 -0800

    function is working on filtering even odd elements out, test still not passing

--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,8 @@ const filter = require('../lib/index');
 
 describe('array methods', () => {
   it('filters out every odd word from an array', () => {
-    const arr = ['sky', 'hot', 'Yeti', 'green'];
-    filter(arr).toEqual(['hot', 'green']);
+    let arr = ['sky', 'hot', 'Yeti', 'green'];
+    let filtered = filter(arr);
+    expect(filtered).toEqual(['hot', 'green']);
   });
 });

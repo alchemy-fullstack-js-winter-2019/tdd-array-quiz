b6ca66c7b4616ef55f43514e305d65404a3345c2
Carmen V Ramos <carmenvramos@yahoo.com>
2019-01-14 11:59:35 -0800

    Added test skeleton

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,9 @@
-
+const { filteredWords } = require('../lib/index');
 
 describe('tdd array quiz', () => {
   it('can take an array of words and returns a filtered array', () => {
     let words = ['sky', 'hot', 'Yeti', 'green'];
     let newWords = filteredWords(words);
-    expect(newWords).toBeEqual(['Is it hot?', 'Is it green?'])
+    expect(newWords).toBeEqual(['Is it hot?', 'Is it green?']);
   });
 });

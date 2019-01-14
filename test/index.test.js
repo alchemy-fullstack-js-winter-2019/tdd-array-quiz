bdcae167c65548b6cdd6ffc27c0d55dc13c7ca54
Dyanna Tolman <dyannast@gmail.com>
2019-01-14 12:17:33 -0800

    fixed an error

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,10 @@
-const replace  = require('../lib/index');
+// const replace  = require('../lib/index');
 
 describe('replace method', () => {
   it('replace strings', () => {
-    const arr1 = ['sky', 'hot', 'Yeti', 'green'].replace(ele => {ele); 
+    const arr1 = ['sky', 'hot', 'Yeti', 'green'].replace(ele => { 
+      return ele;
+    });
     expect(arr1).toEqual(['Is it hot?', 'Is it green?']);
   });
 });
-
-  

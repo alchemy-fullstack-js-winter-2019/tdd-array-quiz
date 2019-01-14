dbfcda32337f696d4b65d15563e7ba87ac7c909d
Masmics <cherlocker3113@gmail.com>
2019-01-14 11:58:54 -0800

    test IP, creating index.js

--- test/index.test.js
+++ test/index.test.js
@@ -1,1 +1,16 @@
-//code here
+const test = require('../lib/index.js');
+
+describe('test', () => {
+
+  const arr = ['sky', 'hot', 'Yeti', 'green'];
+  it('filters out words containing "y"');
+  const itsFiltered = () => {
+    expect(arr.filtered).toEqual('sky', 'green');
+  };
+ 
+  it('returns new string "is it hot, is it green"');
+  const newSentence = () => {
+    expect(arr.sentence).toEqual('Is it hot? Is it green?');
+    done();
+  };  
+});

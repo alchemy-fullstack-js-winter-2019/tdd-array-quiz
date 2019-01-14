27cf4249dd8de7ac5659bd437505c4d074dccf47
Lance Owen Merrill <Lanceom@hawaii.edu>
2019-01-14 11:33:39 -0800

    test expects the correct array but is currently failing

---
+++ test/index.test.js
@@ -0,0 +1,13 @@
+const newSent = require('../lib/index');
+
+describe('test a function to filter and return a new sentence ', () => {
+  it('removes any words with "y" in them and restructures the array to sentence', done => {
+
+    const values = ['sky', 'hot', 'Yeti', 'green'];
+    newSent(values, returnedValue => {
+      expect(returnedValue).toEqual(['Is it hot?', 'Is it green?']);
+      done();
+    });
+  });
+});
+

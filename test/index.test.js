5ac36c97ef93267ad57dabe41f4ad724b7a0bf90
Lance Owen Merrill <Lanceom@hawaii.edu>
2019-01-14 11:57:51 -0800

    returns the wanted array but wont pass test as callback is not a function

--- test/index.test.js
+++ test/index.test.js
@@ -4,8 +4,9 @@ describe('test a function to filter and return a new sentence ', () => {
   it('removes any words with "y" in them and restructures the array to sentence', done => {
 
     const values = ['sky', 'hot', 'Yeti', 'green'];
-    newSent(values, returnedValue => {
-      expect(returnedValue).toEqual(['Is it hot?', 'Is it green?']);
+    newSent(values, () => {
+    //   expect(err).toBeFalsy();
+      expect(newSent(values)).toEqual(['Is it hot?', 'Is it green?']);
       done();
     });
   });
